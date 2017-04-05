ac068ae345edcc899633088fdae568cb960b49ed
delabi <bwire.laban@gmail.com>
2017-04-05 22:02:00 +0300

    Perfect now!

--- word_count2.py
+++ word_count2.py
@@ -4,9 +4,10 @@ from collections import OrderedDict
 
 def words(string):
 
-  my_string = string.split()
-  my_dict = OrderedDict()
-  # my_dict = {}
+  # my_string = string.split()
+  my_string = [int (word) if word.isdigit() else word for word in string.split()]
+  # my_dict = OrderedDict()
+  my_dict  = {}
   for item in my_string:
     my_dict[item] = my_string.count(item)
   print (my_dict)
