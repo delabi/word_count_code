a70ac892f35d8a02985daa2e2d74058830f4a67c
delabi <bwire.laban@gmail.com>
2017-04-05 21:27:19 +0300

    Initial Commit

---
+++ word_count2.py
@@ -0,0 +1,16 @@
+# This Python file uses the following encoding: utf-8
+import os, sys
+from collections import OrderedDict
+
+def words(string):
+
+  my_string = string.split()
+  my_dict = OrderedDict()
+  # my_dict = {}
+  for item in my_string:
+    my_dict[item] = my_string.count(item)
+  print (my_dict)
+  return my_dict
+
+
+words("testing 1 2 testing")
